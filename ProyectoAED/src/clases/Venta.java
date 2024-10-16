2df20848f61301c309e6406fb663106513dc6842
luiscondori98 <lcondori11@hotmail.com>
2024-10-16 16:31:02 -0500

    cargando

--- ProyectoAED/src/clases/Venta.java
+++ ProyectoAED/src/clases/Venta.java
@@ -2,4 +2,65 @@ package clases;
 
 public class Venta {
 
+	private int codigoVenta, codigoCliente, codigoProducto, cantidad;
+	private String fecha;
+	private double precio;
+	
+	public Venta(int codigoVenta, int codigoCliente, int codigoProducto, int cantidad, String fecha, double precio) {
+		
+		this.codigoVenta = codigoVenta;
+		this.codigoCliente = codigoCliente;
+		this.codigoProducto = codigoProducto;
+		this.cantidad = cantidad;
+		this.fecha = fecha;
+		this.precio = precio;
+	}
+
+	public int getCodigoVenta() {
+		return codigoVenta;
+	}
+
+	public void setCodigoVenta(int codigoVenta) {
+		this.codigoVenta = codigoVenta;
+	}
+
+	public int getCodigoCliente() {
+		return codigoCliente;
+	}
+
+	public void setCodigoCliente(int codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+
+	public int getCodigoProducto() {
+		return codigoProducto;
+	}
+
+	public void setCodigoProducto(int codigoProducto) {
+		this.codigoProducto = codigoProducto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 }
--- ProyectoAED/src/module-info.java
+++ ProyectoAED/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module ProyectoAED {
+	requires java.desktop;
 }
